2359971401b4f5112097b7d7e1300424084e5668
kabirrakshe <kabir.rakshe@gmail.com>
2020-07-20 09:45:40 -0700

    Update 1PlayerTicTacToe.py

--- 1PlayerTicTacToe.py
+++ 1PlayerTicTacToe.py
@@ -118,7 +118,7 @@ def Mid_mode(diff):
 ###-----------------------------------------------------------------------------
 def Hard_mode():
     diagonala = (board[0][0],board[1][1],board[2][2])
-    diagonalb = (board[2][0],board[1][1],board[0][2])
+    diagonalb = (board[0][2],board[1][1],board[2][0])
     if diagonala.count(avatarcpu) ==2 and '_' in diagonala:
         fill = diagonala.index('_')
         if fill == 0: diag = 0
